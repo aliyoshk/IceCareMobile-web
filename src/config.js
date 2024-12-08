01685115eaca9042e7841711b3f33dbbdb67112f
Aliyu Khalifa <aliyu.khalifa@wemabank.com>
2024-12-08 13:34:43 +0100

    pointing the right base url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const USE_MOCK_API = false; // Toggle between mock and real API
 // export const API_URL = 'https://localhost:7271/api/';
-export const API_URL = 'https://ice-care-nig-ltd-98a37161d4a2.herokuapp.com/api/';
+export const API_URL = 'https://ice-care-mobile-backend-d5fwe4asg9cwbnbd.southafricanorth-01.azurewebsites.net/api/';
+
