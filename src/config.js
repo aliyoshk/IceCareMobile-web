ea28164b63d14d3b12fa4fa9e14751985e690422
Aliyu Khalifa <aliyu.khalifa@wemabank.com>
2024-12-29 19:58:31 +0100

    adding production url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export const USE_MOCK_API = false; // Toggle between mock and real API
 
 //export const API_URL = 'https://localhost:7271/api/';
-export const API_URL = 'https://ice-care-mobile-backend-d5fwe4asg9cwbnbd.southafricanorth-01.azurewebsites.net/api/';
+//export const API_URL = 'https://ice-care-mobile-backend-d5fwe4asg9cwbnbd.southafricanorth-01.azurewebsites.net/api/';
+export const API_URL = 'https://icecarebackend-g6bshpdjcdhyepde.southafricanorth-01.azurewebsites.net/api/'
 
