6d5021d0e607eb09698859a6b10607874623732f
Aliyu Khalifa <aliyu.khalifa@wemabank.com>
2025-02-10 22:56:52 +0100

    no message

--- src/presentation/store/transferStore.js
+++ src/presentation/store/transferStore.js
@@ -3,13 +3,19 @@ import { defineStore } from 'pinia';
 export const useTransferStore = defineStore('customer', {
   state: () => ({
     selectedCustomer: null,
+    bankDetails: null,
+    receipt: null
   }),
   actions: {
     setSelectedCustomer(customer) {
       this.selectedCustomer = customer;
+      this.bankDetails = customer.bankDetails || null;
+      this.receipt = customer.receipt || null;
     },
     clearSelectedCustomer() {
       this.selectedCustomer = null;
+      this.bankDetails = null;
+      this.receipt = null;
     }
   }
 });
